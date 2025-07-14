16d1d9338b528b302dc3407ede6467c84714c339
ANUJjBhOsAlE <144868481+ANUJjBhOsAlE@users.noreply.github.com>
2025-07-14 17:07:19 +0530

    Delete models directory

--- models/userModel.js
+++
@@ -1,8 +0,0 @@
-const db = require('../config/db');
-
-function registerUser(username, password, callback) {
-  const query = 'INSERT INTO users (username, password) VALUES (?, ?)';
-  db.query(query, [username, password], callback);
-}
-
-module.exports = { registerUser };
